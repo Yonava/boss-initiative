4c4e672a9fd01d734d40ba78df13c87f2c9aff7f
yonava <yona@vossandreae.com>
2023-12-23 11:54:05 -0700

    parallax expe

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="flex items-center justify-center h-[100vh] flex-col">
+    <div class="h-40 w-40 bg-red-600 rounded-full absolute" ref="circle"></div>
   </main>
+  <div class="w-full h-[100vh] bg-blue-500"></div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+
+const circle = ref()
+
+document.addEventListener('scroll', (e) => {
+  if (!circle.value.style) return
+  circle.value.style.scale = window.scrollY / 100
+  circle.value.style.bottom = window.scrollY / 100
+  console.log('scroll')
+})
+</script>
