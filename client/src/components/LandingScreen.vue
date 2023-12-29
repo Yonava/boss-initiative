28972dfe3184cadd939f940e34e4a6e8e74453c8
yonava <yona@vossandreae.com>
2023-12-29 14:53:23 -0700

    rounded action buttons

--- client/src/components/LandingScreen.vue
+++ client/src/components/LandingScreen.vue
@@ -24,7 +24,7 @@
   <div class="w-[100vw] h-[100vh] relative">
 
     <!-- title -->
-    <div class="absolute left-44 top-56 z-20">
+    <div class="absolute left-44 top-44 z-20">
       <h1 class="font-black font-[sans-serif] text-[10rem] leading-[160px] mb-2">
         <span class="bg-gradient-to-r from-red-500 to-blue-600 bg-clip-text text-transparent">
           Boss
@@ -35,6 +35,16 @@
       <h2 class="font-black font-[sans-serif] text-[3rem] leading-[50px] w-[800px] uppercase">
         Beating the odds and striving for success
       </h2>
+
+      <div class="flex gap-5 mt-5">
+        <RoundedButton class="bg-orange-500 text-white">
+          Our Vision
+        </RoundedButton>
+        <RoundedButton class="bg-red-500 text-white">
+          Our Impact
+        </RoundedButton>
+      </div>
+
     </div>
 
     <div class="absolute bottom-0 w-full h-1/2 bg-gradient-to-t from-blue-500 to-white"></div>
@@ -42,6 +52,7 @@
 </template>
 
 <script setup lang="ts">
+import RoundedButton from './RoundedButton.vue'
 import TopNavButton from './TopNavButton.vue';
 const btns = ['Learn More', 'About Us']
 const mainBtn = 'Donate'
---
+++ client/src/components/RoundedButton.vue
@@ -0,0 +1,7 @@
+<template>
+  <button
+    class="w-64 py-5 rounded-full font-[sans-serif] font-extrabold text-3xl uppercase"
+  >
+    <slot><!-- optional fallback --></slot>
+  </button>
+</template>
