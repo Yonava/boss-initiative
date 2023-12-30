3667e1a53012d12dfb456ca2eda1078ab69c09ae
yonava <yona@vossandreae.com>
2023-12-30 14:24:48 -0700

    scrolling with the graphs on the side

--- client/src/components/NHStats.vue
+++ client/src/components/NHStats.vue
@@ -1,31 +1,46 @@
 <template>
   <div>
-    <div class="w-full h-[300vh] relative grid place-items-center bg-blue-500 flex">
-      <div class="absolute top-0 grid place-items-center p-10">
-        <p class="my-5 text-white text-2xl">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor sint laborum laudantium doloremque repellendus ut corporis consectetur? Quisquam amet harum tempore doloremque repudiandae ullam fugiat. Odit quibusdam sequi non laboriosam. Repellendus quidem at blanditiis accusamus consequatur magni ipsa consequuntur ut exercitationem maiores sint ea expedita, amet qui officiis vel ex est? Possimus nostrum veritatis recusandae ipsum aspernatur et saepe? Harum cum sed blanditiis modi officia assumenda explicabo adipisci ipsam, quam incidunt voluptate nisi asperiores, ipsa corrupti, temporibus ab repellendus esse sunt culpa amet perferendis labore atque! Reprehenderit, magni nulla, temporibus animi cupiditate quos quidem aut, hic eveniet eaque impedit nesciunt?
+    <div class="w-full h-[400vh] relative bg-blue-500 flex">
+      <div class="absolute top-0 w-full grid place-items-center">
+        <p class="mt-[100px] text-white text-8xl uppercase w-1/2 text-center">
+          Facing our issues <span class="font-extrabold">head on</span>
         </p>
         <img
-          class="w-[800px]"
+          class="w-[800px] mb-[200px]"
           src="/nh.svg"
           ref="imgAbs"
         />
+        <div class="w-full flex px-10 justify-center">
+          <div class="border-2 border-black w-1/3 grid place-items-center">
+            <img
+              src="../assets/pie.jpeg"
+              alt=""
+            >
+          </div>
+          <div class="w-1/3"></div>
+          <div class="border-2 border-black w-1/3 grid place-items-center">
+            <img
+              src="../assets/dot.jpeg"
+              alt=""
+            >
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
+import { ref } from 'vue'
+
+const range = {
+  y: [1250, 3000],
+  scale: [1, 1],
+}
 
 const imgAbs = ref()
 const yOffset = ref(0)
-const lastScrollY = ref(1400)
-
-const range = {
-  y: [1400, 2000],
-  scale: [1, 15],
-}
+const lastScrollY = ref(range.y[0])
 
 document.addEventListener('scroll', () => {
   // if not in range, do nothing
