b8f5fae00b53ae92ae53788a9776bdbdeb3e63c1
yonava <yona@vossandreae.com>
2024-01-01 23:09:27 -0700

    slide show scroll

---
+++ client/src/components/ImagePresenter.vue
@@ -0,0 +1,23 @@
+<template>
+  <img
+    class="object-fit"
+    :src="image"
+  >
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const props = defineProps<{
+  imgs: string[],
+  range: number[],
+}>()
+
+const image = ref('')
+
+document.addEventListener('scroll', () => {
+  const index = props.range.findIndex((r) => window.scrollY < r)
+  if (index === -1) return (image.value = '')
+  image.value = props.imgs[index]
+})
+</script>
