120fe33f709a90f38f335dc25352530b174589f3
yonava <yona@vossandreae.com>
2024-01-05 13:34:56 -0800

    reset prison pop to default

--- client/src/components/NHPrisonPopulation.vue
+++ client/src/components/NHPrisonPopulation.vue
@@ -4,20 +4,24 @@
       Number of People in New Hampshire's Criminal System
     </h1>
     <apexchart
+      @dataPointMouseEnter="setter"
+      @mouseleave="setSelectedPrisonPopulation(null)"
       width="700"
       type="pie"
-      @dataPointMouseEnter="setter"
       :series="series"
       :options="PieChartOptions"
     ></apexchart>
     <div class="flex flex-wrap gap-4 justify-center">
       <div
-        v-for="entry in prisonPopulation"
+        v-for="population in prisonPopulation"
+        :key="population.name"
+        @mouseenter="setSelectedPrisonPopulation(population)"
+        @mouseleave="setSelectedPrisonPopulation(null)"
         class="flex bg-blue-600 px-3 py-2 rounded-lg items-center hover:bg-blue-700 cursor-pointer"
       >
-        <div :class="`w-6 h-6 bg-[${entry.color}] rounded-full mr-2 mt-1`"></div>
+        <div :class="`w-6 h-6 bg-[${population.color}] rounded-full mr-2 mt-1`"></div>
         <p class="text-white text-2xl font-bold text-center">
-          {{ entry.name }}
+          {{ population.name }}
         </p>
       </div>
     </div>
