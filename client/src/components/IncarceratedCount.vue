aea74e3847e5a532ea9f26ad3326140dcc53628f
yonava <yona@vossandreae.com>
2024-01-15 16:09:10 -0800

    flakes falling on spawn points

--- client/src/components/IncarceratedCount.vue
+++ client/src/components/IncarceratedCount.vue
@@ -3,6 +3,15 @@
 
     <div ref="spawn"></div>
 
+    <div class="absolute w-full h-full flex justify-between px-12 z-20">
+      <div
+        v-for="i in 10"
+        :key="i"
+        :ref="el => barLocations.push(el)"
+        class="w-[50px] h-[50px] -translate-y-32"
+      ></div>
+    </div>
+
     <div class="w-full text-gray-100 grid place-items-center">
       <div class="grid place-items-center pb-8">
         <h1 class="text-[12rem] font-extrabold leading-0">
@@ -28,17 +37,27 @@ const count = ref(2_743_400)
 
 const spawn = ref()
 
+const barLocations = ref<any[]>([])
+
 const getRandomInt = (min: number, max: number) => {
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
+const getRandomBarLocation = () => {
+  const randomIndex = getRandomInt(0, barLocations.value.length - 1)
+  const barRef = barLocations.value[randomIndex]
+  const rect = barRef?.getBoundingClientRect()
+  console.log(rect?.x)
+  return rect?.x
+}
+
 const spawnParticle = () => {
-  const classes = ['h-12', 'w-12', 'absolute', 'top-0', 'bg-gradient-to-br', 'rounded-full']
+  const classes = ['h-12', 'w-12', 'absolute', '-top-28', 'bg-gradient-to-br', 'rounded-full']
   const colors = [['from-blue-900', 'to-gray-900'], ['from-red-900', 'to-gray-900'], ['from-blue-600', 'to-gray-900'], ['from-red-500', 'to-blue-500'], ['from-blue-800', 'to-gray-600']]
   const color = colors[getRandomInt(0, colors.length - 1)]
-  const randomX = getRandomInt(5, 95)
+  const randomX = getRandomBarLocation()
   const particle = document.createElement('div')
-  particle.classList.add(...classes, `left-[${randomX}%]`, ...color)
+  particle.classList.add(...classes, `left-[${randomX}px]`, ...color)
   spawn.value?.appendChild(particle)
 
   particle.animate([
